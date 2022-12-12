ef80764ec54e46fca7eb497a3e0f4709595b0464
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-12-12 13:12:20 +0300

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,22 +7,53 @@ import java.io.FileInputStream
 
 
 fun main() {
+
+    val action = "action"
+    val content = "content"
+
     val options = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
         .build()
 
     FirebaseApp.initializeApp(options)
 
-    val message = Message.builder()
-        .putData("action", "LIKE")
-        .putData("content", """{
+    val newLike = Message.builder()
+        .putData(action, "LIKE")
+        .putData(
+            content, """{
           "userId": 1,
           "userName": "Vasiliy",
           "postId": 2,
           "postAuthor": "Netology"
-        }""".trimIndent())
+        }""".trimIndent()
+        )
         .setToken(token)
         .build()
 
-    FirebaseMessaging.getInstance().send(message)
+    val newPost = Message.builder()
+        .putData(action, "NEW_POST")
+        .putData(
+            content, """{
+          "userId": 1,
+          "userName": "Android",
+          "content": "New information"
+        }""".trimIndent()
+        )
+        .setToken(token)
+        .build()
+
+    val update = Message.builder()
+        .putData(action, "UPDATE")
+        .putData(
+            content, """{
+          "userId": 1,
+          "userName": "Vasiliy"
+        }""".trimIndent()
+        )
+        .setToken(token)
+        .build()
+
+    FirebaseMessaging.getInstance().send(newLike)
+    FirebaseMessaging.getInstance().send(newPost)
+    FirebaseMessaging.getInstance().send(update)
 }
